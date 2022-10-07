b1092974cf5b8880b5d6d5ce4756958846dbd759
appredator <volbass92@gmail.com>
2022-10-06 22:13:03 -0500

    5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let detectTypeA = typeof a
+    let detectTypeB = typeof b
 
+    if(detectTypeA != "number" || detectTypeB != "number" || a < 0 || b < 0){
+        sum = "ERROR"
+        
+    }
+    else{
+    if(a < b){
+    for (let i = a; i <= b; i++)
+    {
+        sum = sum + i
+    }
+    }   
+    else if (a > b){
+    for (let i = b; i <= a; i++)
+        {
+            sum = sum + i
+        }
+    }
+}
+    return sum;
 };
 
 // Do not edit below this line
