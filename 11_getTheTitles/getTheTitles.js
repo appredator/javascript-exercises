654022c13a783adbe8c6a24e6ef3d8a0811f05b0
appredator <volbass92@gmail.com>
2022-10-28 19:43:39 -0500

    More work

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+let holder = new Array(arr.length - 1)
+holder.fill(0);
 
+for (let i = 0; i < arr.length; i++){
+
+    holder[i] = arr[i].title;
+}
+
+return holder;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,60 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+   const holder = people.filter(people => !people.yearOfDeath) 
+
+    // const currentDate = getFullYear();
+
+    const currentDate = 2022;
+
+    let carlyBirth = 0;
+
+    let rayBirth = 0;
+
+    let janeBirth = 0;
+
+        let largest = 0;
+
+
+    for(let i = 0; i < people.length - 1; i++){
+    currentDate - people.yearOfBirth;
+
+    if(people[i].name === "Carly"){
+    carlyBirth = currentDate - people[i].yearOfBirth;
+    }
+    else if( people[i].name === "Ray"){
+        rayBirth = currentDate - people[i].yearOfBirth;
+    }
+    else if( people[i].name === "Jane"){
+        janeBirth = currentDate - people[i].yearOfBirth;
+    }
+    }
+
+    if(carlyBirth > rayBirth){
+        largest = carlyBirth;
+    }
+    else{
+        largest = rayBirth;
+    }
+
+
+if (largest > janeBirth){
+
+    
+}
+else {
+    largest = janeBirth;
+}
+
+
+if (carlyBirth === largest){
+    return people.filter(name => people.name === "Carly")
+}
+else if (rayBirth === largest){
+    return people.filter(name => people.name === "Ray")
+}
+else if (rayBirth === largest){
+    return people.filter(name => people.name === "Jane")
+}
 };
 
 // Do not edit below this line
